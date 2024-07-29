e82f6f79748919b0e2e6b55b958fb0edd1f3e3ea
elahelahe <elaheshojaeehabibzadeh@gmail.com>
2024-07-29 18:49:39 +0330

    fix: Dataset preprocessing +reorder labels list and tracing list +select 6 heart disease columns +remove last index in ids which is empty in tracings

--- datasets.py
+++ datasets.py
@@ -19,15 +19,51 @@ class ECGSequence(Sequence):
         if path_to_csv is None:
             self.y = None
         else:
-            self.y = pd.read_csv(path_to_csv).values
+            y = pd.read_csv(path_to_csv, index_col="exam_id")
+            self.y = y.astype(np.float32)
+
         # Get tracings
         self.f = h5py.File(path_to_hdf5, "r")
+
+        # list all datasets in hdf5 file
+        with self.f as hdf:
+            # Recursively list all datasets in the file
+            def list_datasets(name, obj):
+                if isinstance(obj, h5py.Dataset):
+                    print(name)
+            hdf.visititems(list_datasets)
+
+        # get tracings and remove last index which is empty
         self.x = self.f[hdf5_dset]
+        self.x = self.x[:-1]
+
+        # list of exam_id's of tracings
+        tracing_ids = self.f['exam_id']
+
+        # sort labels to match tracings index (they are not in same order)
+        df = self.y.reindex(tracing_ids, fill_value=False, copy=True)
+        self.y = df
+
+        # eliminiate unnecessary columns
+        self.y = self.y[['1dAVb', 'RBBB', 'LBBB', 'SB', 'ST', 'AF']].values
+
+        # get tracings and remove last index which is empty
+        self.y = self.y[:-1]
+        self.y = self.y.astype(np.float32)
+
         self.batch_size = batch_size
         if end_idx is None:
             end_idx = len(self.x)
         self.start_idx = start_idx
         self.end_idx = end_idx
+
+        # Print shapes of x and y
+        print(f"Shape of x: {self.x.shape}")
+        if self.y is not None:
+            print(f"Shape of y: {self.y.shape}")
+
+        print(tracing_ids[0], 'first index in hdf5 file')
+        print("First data sample in x:", self.x[0])
 
     @property
     def n_classes(self):
